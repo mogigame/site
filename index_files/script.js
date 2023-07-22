c5229b7627a50d2e0d9db8692ac5a8e4e9e3f138
mogigameYTB <lili24082008@gmail.com>
2023-07-23 00:51:15 +0200

    css assembler même fichier ||| site bot discord au lieu de serveur minecraft

--- index_files/script.js
+++
@@ -1,6 +0,0 @@
-let ip = document.getElementById('ip');
-
-ip.addEventListener('click', () => {
-    navigator.clipboard.writeText('neocraft1293.fr');
-    window.alert("Tu as bien copié l'IP du serveur ")
-})
